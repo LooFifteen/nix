224ebb3c4509515e145314ed62c72b90e26c29d1
Luis <luis@lu15.dev>
2025-07-02 16:20:39 +0100

    feat(easyeffets): add MORE BASS

--- systems/david/configuration.nix
+++ systems/david/configuration.nix
@@ -48,6 +48,9 @@
   # games
   programs.gamemode.enable = true;
 
+  # easyeffects
+  programs.dconf.enable = true;
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
